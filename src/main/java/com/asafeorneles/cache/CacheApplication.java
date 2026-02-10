1e175a1ac329668f650a52e226fc1214ea9aad9c
Asafe Orneles <asafeorneles@gmail.com>
2026-02-10 18:56:38 -0300

    feat: Permitindo o uso de FeingClients no projeto.

--- src/main/java/com/asafeorneles/cache/CacheApplication.java
+++ src/main/java/com/asafeorneles/cache/CacheApplication.java
@@ -3,11 +3,13 @@ package com.asafeorneles.cache;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableCaching
 @EnableScheduling
+@EnableFeignClients
 public class CacheApplication {
 
 	public static void main(String[] args) {
